b856314296cb1b55b9a13d2619ef006360b924b3
Christian Miller <cwgmiller@gmail.com>
2015-06-18 10:24:52 +0100

    New test added for responding to place

---
+++ lib/board.rb
@@ -0,0 +1,8 @@
+class Board 
+
+	attr_reader :ships
+	
+	def initialize 
+		@ships = []
+	end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,14 @@
+require 'board'
+
 describe Board do
-  it 'creates an array' do
-    expect(subject).to eq []
-  end
+  
+  context 'when created' do
+ 		it 'creates an array' do
+    	expect(subject.ships).to eq []
+  	end
+	end	
+
+	it 'responds to place' do 
+		expect(subject).to repsond_to :place
+	end
 end

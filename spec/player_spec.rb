b4f2264c77737d39ea67d2f4ad32b472f6067ee3
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-18 16:05:09 +0100

    added new test to check if player can place a ship

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,21 @@
 require 'player'
 
 describe Player do
+
+  let (:board) {double :board}
+  subject {Player.new board}
+
   describe 'lose' do
     let (:board) {double :board, all_ships_sunk?: true}
     subject {Player.new board}
     it 'can lose' do
       expect(subject).to be_lost
     end
+  end
+
+  it 'can place ships' do
+    ship = double :ship
+    expect(board).to receive :place
+    subject.place(ship)
   end
 end

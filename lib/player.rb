f87665f0e150af7a22dce099bfcf60337d05e290
Christian Miller <cwgmiller@gmail.com>
2015-06-18 12:35:30 +0100

    Player can lose

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,12 @@
 class Player
+
+attr_reader :board
+
+def initialize board
+	@board = board
+end
+
+def lost?
+ board.all_ships_sunk?
+end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,8 @@
 require 'player'
 
 describe Player do
-  context 'lose' do
-    board = double :board, all_ships_sunk?: true
+  describe 'lose' do
+    let (:board) {double :board, all_ships_sunk?: true}
     subject {Player.new board}
     it 'can lose' do
       expect(subject).to be_lost

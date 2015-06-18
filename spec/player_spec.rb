0edadfd36ae9af36934128fa642c5d3bfb735842
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-18 12:31:49 +0100

    added a new test to check if player can lose

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,11 @@
 require 'player'
 
-describe Player do 
+describe Player do
+  context 'lose' do
+    board = double :board, all_ships_sunk?: true
+    subject {Player.new board}
+    it 'can lose' do
+      expect(subject).to be_lost
+    end
+  end
 end
